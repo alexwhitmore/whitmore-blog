a0d91423761115a30ee1286e81b6ce09301f71c0
alexwhitmore <heyimalexw@gmail.com>
2024-01-13 18:09:12 -0700

    Clean up, finish writing blog post

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,5 @@
 import { ModeToggle } from './mode-toggle'
 
 export function Footer() {
-  return (
-    <footer className='flex justify-center pt-10'>
-      <ModeToggle />
-    </footer>
-  )
+  return <footer className='flex justify-center pt-10'>{/* <ModeToggle /> */}</footer>
 }
